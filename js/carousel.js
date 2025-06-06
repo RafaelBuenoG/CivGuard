6720f874afd1f2badc04b2a2b7d3f81525938fc8
Rafael Bueno <rafaelbuenog2020@gmail.com>
2025-06-06 16:22:58 -0300

    First Commit

---
+++ js/carousel.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelectorAll(".carousel-container");
+    const btnLeft = document.querySelectorAll(".left");
+    const btnRight = document.querySelectorAll(".right");
+
+    const scrollAmount = 300;
+
+    for (let i = 0; i <= carousel.length; i++){
+        btnLeft[i].addEventListener("click", () => {
+            carousel[i].scrollBy({
+                left: -scrollAmount,
+                behavior: "smooth"
+            });
+        });
+
+        btnRight[i].addEventListener("click", () => {
+            carousel[i].scrollBy({
+                left: scrollAmount,
+                behavior: "smooth"
+            });
+        });
+    }
+});
